6c5d97413305a07776d43c05fabfcc3cb7e47694
StudentGD <gbrunodalmolin@gmail.com>
2021-02-07 16:12:06 -0300

    Adicionado Controle de Umidade
    Adicionando constantes de umidade para futuramente definir as instruções de controle.

--- main.ino
+++ main.ino
@@ -3,12 +3,15 @@
 #define DHTPIN A1 //Definindo o pino para dados do DHT11.
 #define DHTTYPE DHT11 //Definindo que o modelo do sensor DHT sera o 11.
 
-#define MaxBee 32 //Definindo valor maximo de temperatura para as abelhas.
-#define MinBee 25 //Definindo o valor minimo de temperatura para as abelhas.
+#define MaxTempBee 32 //Definindo valor maximo de temperatura para as abelhas.
+#define MinTempBee 25 //Definindo o valor minimo de temperatura para as abelhas.
+
+#define MaxHumBee 60  //Definindo valor maximo de umidade para as abelhas.
+#define MinHumBee 50  //Definindo o valor minimo de umidade para as abelhas.
 
 DHT dht(DHTPIN, DHTTYPE); //Atribuindo os parametros para a função referente ao DHT.
 
-void setup(){
+void setup() {
 
     Serial.begin(9600); //Inicia o monitor serial do arduino com um baud rate de 9600 bits por segundo para o DHT11.
 
@@ -17,7 +20,7 @@ void setup(){
     Serial.println("DHT11 Iniciou!"); 
 }
 
-void loop(){
+void loop() {
 
     float u = dht.readHumidity(); //Atribuindo o valor da umidade do sensor DHT11 na variavel u.
     float t = dht.readTemperature(); //Atribuindo o valor da temperatura do sensor DHT11 na variavel t.
@@ -35,13 +38,24 @@ void loop(){
         Serial.print("Umidade: ");
         Serial.print(u);  //Apresentando dados da umidade no Monitor serial.
         Serial.println("%");
-        delay(1000);
 
-        if(t<MinBee){
+        delay(1000);  //Delay de 1 segundo.
+
+        if(t < MinTempBee) {
 
             //Adicionar instruções de Controle.
 
-        } else if(t>MaxBee){
+        } else if(t > MaxTempBee) {
+
+            //Adicionar instruções de Controle.
+
+        }
+
+        if(u < MinHumBee) {
+
+            //Adicionar instruções de Controle.
+
+        } else if(u > MaxHumBee) {
 
             //Adicionar instruções de Controle.
 
